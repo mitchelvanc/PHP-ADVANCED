885cc1218f74c791e9b79923a0675d55656372fe
mitchel van cronenburg <mitchel.vancronenburg@student.rocva.nl>
2021-03-17 13:18:41 +0100

    klaar

---
+++ 2-Loops/taak01/loops-while.php
@@ -0,0 +1,24 @@
+<?php
+
+$leeftijd = 0;
+
+
+while($leeftijd <18){
+    echo "ik ben $leeftijd jaar, dus ik mag nog niet stemmen <br>";
+    $leeftijd++;}
+
+while($leeftijd >=18){
+    echo "ik ben $leeftijd jaar, dus ik mag stemmen <br>";
+    $leeftijd++;
+}
+
+
+
+
+
+
+
+
+
+
+?>
