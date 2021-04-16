2d3a024d590cac3768069145d1304a417f82f0ee
mitchel van cronenburg <mitchel.vancronenburg@student.rocva.nl>
2021-04-16 09:50:08 +0200

    gedaan

---
+++ 3-Forms/taak03/mijnmuziek.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title></title>
+    </head>
+    <body>
+
+
+
+        <?php
+
+        echo "artiest is:".$_GET["artiest"];
+        echo "<br>";
+        echo "titel is: ".$_GET["titel"];
+        echo "<br>";
+        echo "jaar van uitkomst is: ".$_GET["jaarvanuitkomst"];
+        echo "<br>";
+        echo "muzieksoort is: ".$_GET["muzieksoort"];
+
+        ?>
+    </body>
+</html>
